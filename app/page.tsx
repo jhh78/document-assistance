73dc6d5861c03959118ba84caaeba622fe98f65d
鄭會憲 <vkcel400@gmail.com>
2025-02-26 20:59:46 +0900

    패치문제 해결

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,24 @@ import { Metadata, NextPage } from "next";
 import styles from "./page.module.css";
 import { BASE_API_URL } from "@/lib/constant/common";
 
-export async function generateMetadata(): Promise<Metadata> {
-  // fetch data
-  const response = await fetch(`${BASE_API_URL}/meta`);
+export async function generateMetadata(): Promise<Metadata | null> {
+  try {
+    // fetch data
+    const response = await fetch(`${BASE_API_URL}/meta`);
 
-  if (!response.ok) {
-    throw new Error(`Failed to fetch data: ${response.statusText}`);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch data: ${response.statusText}`);
+    }
+
+    const product = await response.json();
+
+    return {
+      title: product.title,
+      description: product.description,
+    };
+  } catch {
+    return null;
   }
-
-  const product = await response.json();
-
-  return {
-    title: product.title,
-    description: product.description,
-  };
 }
 
 const Page: NextPage = () => {
