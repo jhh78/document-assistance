142009734d5642488acd2dc3bf7a5a5f667aca28
鄭會憲 <vkcel400@gmail.com>
2025-02-28 07:52:25 +0900

    yarn update

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,10 @@
 import { Metadata, NextPage } from "next";
 import styles from "./page.module.css";
-import { BASE_API_URL } from "@/lib/constant/common";
 
-export async function generateMetadata(): Promise<Metadata | null> {
-  try {
-    // fetch data
-    const response = await fetch(`${BASE_API_URL}/meta`);
-
-    if (!response.ok) {
-      throw new Error(`Failed to fetch data: ${response.statusText}`);
-    }
-
-    const product = await response.json();
-
-    return {
-      title: product.title,
-      description: product.description,
-    };
-  } catch {
-    return null;
-  }
-}
+export const metadata: Metadata = {
+  title: "Document Support",
+  description: "This is a test page for Vercel deployment",
+};
 
 const Page: NextPage = () => {
   return (
