8488e45bf75bc8a197c6e67ac58c7d5752b56951
鄭會憲 <vkcel400@gmail.com>
2025-03-04 20:25:22 +0900

    애널리틱스 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 import { Metadata } from "next";
 
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
