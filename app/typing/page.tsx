3259003bb9c1648046c4412023bd276818f94a84
鄭會憲 <vkcel400@gmail.com>
2025-03-06 20:56:58 +0900

    스토어 초기화 문제 해결

--- app/typing/page.tsx
+++ app/typing/page.tsx
@@ -1,12 +1,19 @@
 "use client";
 import { NextPage } from "next";
-import React from "react";
+import React, { useEffect } from "react";
 import Game from "@/component/typing/Game";
 import FileUploadForm from "@/component/typing/FileUploadForm";
 import { GAME_PAGE, TITLE_PAGE, useTypingGameStore } from "@/stores/typing";
 
 const Page: NextPage = () => {
   const screenMode = useTypingGameStore((state) => state.screenMode);
+  const initStore = useTypingGameStore((state) => state.initStore);
+
+  useEffect(() => {
+    return () => {
+      initStore();
+    };
+  }, []);
 
   return (
     <React.Fragment>
